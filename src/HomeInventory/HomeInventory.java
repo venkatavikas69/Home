279162676d96cfd8f04fab16542afe11c8c9a8e1
venkatavikas69 <venkatavikas69@gmail.com>
2021-02-03 18:13:38 +0530

    added extra features

--- src/HomeInventory/HomeInventory.java
+++ src/HomeInventory/HomeInventory.java
@@ -3,6 +3,9 @@ package HomeInventory;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Calendar;
+import com.toedter.calendar.*;
+import java.beans.*;
 
 public class HomeInventory extends JFrame {
 
@@ -14,6 +17,24 @@ public class HomeInventory extends JFrame {
 	JButton nextButton = new JButton(new ImageIcon("C:\\Users\\Vikas Ravi\\Downloads\\New Size\\next.gif"));
 	JButton printButton = new JButton(new ImageIcon("C:\\Users\\Vikas Ravi\\Downloads\\New Size\\print.gif"));
 	JButton exitButton = new JButton();
+	JLabel itemLabel = new JLabel();
+	JTextField itemTextField = new JTextField();
+	JLabel locationLabel = new JLabel();
+	JComboBox locationComboBox = new JComboBox();
+	JCheckBox markedCheckBox = new JCheckBox();
+	JLabel serialLabel = new JLabel();
+	JTextField serialTextField = new JTextField();
+	JLabel priceLabel = new JLabel();
+	JTextField priceTextField = new JTextField();
+	JLabel dateLabel = new JLabel();
+	JDateChooser dateDateChooser = new JDateChooser();
+	JLabel storeLabel = new JLabel();
+	JTextField storeTextField = new JTextField();
+	JLabel noteLabel = new JLabel();
+	JTextField noteTextField = new JTextField();
+	JLabel photoLabel = new JLabel();
+	JTextArea photoTextArea = new JTextArea();
+	JButton photoButton = new JButton();
 
 	public static void main(String[] args) {
 		new HomeInventory();
@@ -96,6 +117,168 @@ public class HomeInventory extends JFrame {
 		exitButton.setToolTipText("Exit Program");
 		inventoryToolBar.add(exitButton);
 		exitButton.addActionListener(e -> exitButtonActionPerformed(e));
+		
+		itemLabel.setText("Inventory Item");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 0;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(itemLabel, gridConstraints);
+		
+		itemTextField.setPreferredSize(new Dimension(400, 25));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 0;
+		gridConstraints.gridwidth = 5;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(itemTextField, gridConstraints);
+		
+		locationLabel.setText("Location");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 1;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(locationLabel, gridConstraints);
+		
+		locationComboBox.setPreferredSize(new Dimension(270, 25));
+		locationComboBox.setFont(new Font("Arial", Font.PLAIN, 12));
+		locationComboBox.setEditable(true);
+		locationComboBox.setBackground(Color.WHITE);
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 1;
+		gridConstraints.gridwidth = 3;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(locationComboBox, gridConstraints);
+		
+		markedCheckBox.setText("Marked?");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 5;
+		gridConstraints.gridy = 1;
+		gridConstraints.insets = new Insets(10, 10, 0, 0);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(markedCheckBox, gridConstraints);
+		
+		serialLabel.setText("Serial Number");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 2;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(serialLabel, gridConstraints);
+		
+		serialTextField.setPreferredSize(new Dimension(270, 25));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 2;
+		gridConstraints.gridwidth = 3;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(serialTextField, gridConstraints);
+		
+		priceLabel.setText("Purchase Price");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 3;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(priceLabel, gridConstraints);
+		
+		priceTextField.setPreferredSize(new Dimension(160,25));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 3;
+		gridConstraints.gridwidth = 2;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(priceTextField, gridConstraints);
+		
+		dateLabel.setText("Date Purchased");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 4;
+		gridConstraints.gridy = 3;
+		gridConstraints.insets = new Insets(10, 10, 0, 0);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(dateLabel, gridConstraints);
+		
+		dateDateChooser.setPreferredSize(new Dimension(120, 25));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 5;
+		gridConstraints.gridy = 3;
+		gridConstraints.gridwidth = 2;
+		gridConstraints.insets = new Insets(10,0,0,10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(dateDateChooser, gridConstraints);
+		
+		storeLabel.setText("Store/Website");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 4;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(storeLabel, gridConstraints);
+		
+		storeTextField.setPreferredSize(new Dimension(400, 25));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 4;
+		gridConstraints.gridwidth = 5;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(storeTextField, gridConstraints);
+		
+		noteLabel.setText("Note");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 5;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(noteLabel, gridConstraints);
+		
+		noteTextField.setPreferredSize(new Dimension(400, 25));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 5;
+		gridConstraints.gridwidth = 5;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(noteTextField, gridConstraints);
+		
+		photoLabel.setText("Photo");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 1;
+		gridConstraints.gridy = 6;
+		gridConstraints.insets = new Insets(10, 10, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.EAST;
+		getContentPane().add(photoLabel, gridConstraints);
+		
+		photoTextArea.setPreferredSize(new Dimension(350, 35));
+		photoTextArea.setFont(new Font("Arial", Font.PLAIN, 12));
+		photoTextArea.setEditable(false);
+		photoTextArea.setLineWrap(true);
+		photoTextArea.setWrapStyleWord(true);
+		photoTextArea.setBackground(new Color(255,255,192));
+		photoTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 2;
+		gridConstraints.gridy = 6;
+		gridConstraints.gridwidth = 4;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(photoTextArea, gridConstraints);
+		
+		photoButton.setText("...");
+		gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = 6;
+		gridConstraints.gridy = 6;
+		gridConstraints.insets = new Insets(10, 0, 0, 10);
+		gridConstraints.anchor = GridBagConstraints.WEST;
+		getContentPane().add(photoButton, gridConstraints);
+		photoButton.addActionListener(e -> photoButtonActionPerformed(e));
 
 		pack();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -135,6 +318,10 @@ public class HomeInventory extends JFrame {
 
 	private void exitButtonActionPerformed(ActionEvent e) {
 
+	}
+	
+	private void photoButtonActionPerformed(ActionEvent e) {
+		
 	}
 
 	private void exitForm(WindowEvent evt) {
